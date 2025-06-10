cdf04eede9a8c61611927f06fc8321cfe7a5ad58
James Grieve <jamesonrgrieve@gmail.com>
2025-06-09 19:21:43 -0600

    Pagination and url slug variables;

--- src/ProjectName/Controllers/PersonController.cs
+++ src/ProjectName/Controllers/PersonController.cs
@@ -5,35 +5,73 @@ namespace ProjectName.Controllers
     [ApiController]
     public class PersonController : ControllerBase
     {
+        static string[] People = new string[] {
+            "Liam",
+            "Olivia",
+            "Noah",
+            "Emma",
+            "Oliver",
+            "Ava",
+            "Elijah",
+            "Sophia",
+            "James",
+            "Isabella",
+            "William",
+            "Mia",
+            "Benjamin",
+            "Charlotte",
+            "Lucas",
+            "Amelia",
+            "Henry",
+            "Harper",
+            "Alexander",
+            "Evelyn",
+            "Daniel",
+            "Abigail",
+            "Matthew",
+            "Emily",
+            "Sebastian",
+            "Ella",
+            "Jack",
+            "Elizabeth",
+            "Samuel",
+            "Camila",
+            "David",
+            "Luna",
+            "Joseph",
+            "Sofia",
+            "Carter",
+            "Avery",
+            "Owen",
+            "Scarlett",
+            "Wyatt",
+            "Grace",
+            "John",
+            "Chloe",
+            "Leo",
+            "Victoria",
+            "Luke",
+            "Riley",
+            "Julian",
+            "Aria",
+            "Grayson",
+            "Lily"
+        };
         [HttpGet("")]
-        public ActionResult<IEnumerable<string>> GetPeople()
+        public ActionResult<IEnumerable<string>> GetPeople(int page, int pageSize, bool reverse)
         {
-            return new string[] {
-                "John",
-                "Joe",
-                "Bob",
-                "Sue"
-            };
+            string[] sortedPeople = (reverse ? People.OrderByDescending(x => x) : People.OrderBy(x => x)).ToArray();
+            return sortedPeople.Skip((page - 1) * pageSize).Take(pageSize).ToArray();
         }
         [HttpGet("all")]
         public ActionResult<IEnumerable<string>> GetAllPeople()
         {
-            return new string[] {
-                "John",
-                "Joe",
-                "Bob",
-                "Sue"
-            };
+            return People;
         }
-        [HttpPatch("")]
-        public ActionResult<IEnumerable<string>> PatchPeople()
+        [HttpGet("{name}")]
+        public ActionResult<int> GetListPosition(string name)
         {
-            return new string[] {
-                "John",
-                "Joe",
-                "Bob",
-                "Sue"
-            };
+            return 1 + Array.IndexOf(People.OrderBy(x => x).ToArray(), name);
         }
     }
 }
